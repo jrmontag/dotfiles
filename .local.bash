a260029b7c13796ffceef7a324026c33b4718f4e
Josh Montague <joshua.montague@gmail.com>
2018-04-17 13:18:38 -0600

    update .local.bash for anaconda

--- .local.bash
+++ .local.bash
@@ -7,30 +7,28 @@
 # clear ruby error from twurl
 # http://stackoverflow.com/questions/20803933/turn-off-ruby-2-1-deprecated-ruby-heap-min-slots-warning
 unset RUBY_HEAP_MIN_SLOTS
+ 
+# be sure to use vim
+export EDITOR=$(which vim)
 
 #   / misc 
 ######### 
-
-# autojump
-# $ brew install autojump
-#[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
 
 ######### 
 #   python 
 
-# virtualenvwrapper setup ( 2015-01-23 )
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
+# virtualenvwrapper setup (2015)
+#export WORKON_HOME=$HOME/.virtualenvs
+#export PROJECT_HOME=$HOME/Devel
+#source /usr/local/bin/virtualenvwrapper.sh
 
-# from the python bootstrap, per install instructions ( 2015-01-23 )
-#PATH=/Users/jmontague/Python/PyPy-2.2.1/bin:$PATH
+## anaconda (2017)
+source /Users/jmontague/anaconda3/etc/profile.d/conda.sh
 
 #   / python
 ########
 
 
-# check for aliases ( 2015-01-22 )
+# check for aliases 
 [[ -s ${HOME}/.alias.bash ]] && source ${HOME}/.alias.bash
-
